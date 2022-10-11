d14a048a4d7673a0a29042cf03fa6b99a3606f6b
MD.AL-AMIN <mdalamin67378@gmailcom>
2022-10-12 01:48:13 +0600

    added book animation

--- js/index.js
+++ js/index.js
@@ -451,3 +451,39 @@ function hoverAnimation() {
 
 	init();
 };
+
+/*============================
+	OldBook Page Start Here
+==============================*/
+function myFunction() {
+	var dots = document.getElementById("dots");
+	var moreText = document.getElementById("more");
+	var btnText = document.getElementById("myBtn");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Read more";
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Read less";
+	  moreText.style.display = "inline";
+	}
+  }
+/*============================
+	OldBook Page End Here
+==============================*/
+
+
+/*============================
+	Card Page Start Here
+==============================*/
+/* Demo purposes only */
+$(".hover").mouseleave(
+	function() {
+	  $(this).removeClass("hover");
+	}
+  );
+/*============================
+	Card Page End Here
+==============================*/
