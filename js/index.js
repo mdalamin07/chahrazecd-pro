2db1885dbbf4d105a2c41daeabda5390d1a69336
MD.AL-AMIN <mdalamin67378@gmailcom>
2022-10-19 17:23:54 +0600

    added preloder and animation

--- js/index.js
+++ js/index.js
@@ -487,3 +487,17 @@ $(".hover").mouseleave(
 /*============================
 	Card Page End Here
 ==============================*/
+
+/*============================
+	Preloader Start Here
+==============================*/
+$(window).load(function() { // makes sure the whole site is loaded
+    $("#status").fadeOut(); // will first fade out the loading animation
+    $("#preloader").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
+})
+/*============================
+	Preloader End Here
+==============================*/
+
+
+
